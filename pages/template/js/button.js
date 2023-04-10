0364eaabfdfeccd4277bba7b93d3facce072ce65
xiaobai_mushroom_soup <88175748+xiaobai-mushroom-soup@users.noreply.github.com>
2023-04-10 19:08:51 +0800

    feat:finish template (#1)
    Co-authored-by: v_runzhiguo <v_runzhiguo@tencent.com>

---
+++ pages/template/js/button.js
@@ -0,0 +1,12 @@
+(function () {
+  const domList = document.querySelectorAll(".t__button");
+  domList.forEach((dom) => {
+    const { paddingLeft } = getComputedStyle(dom);
+    const padding = parseFloat(paddingLeft) || 0;
+    if (dom.scrollWidth > dom.offsetWidth) {
+      dom.style.fontSize = `${
+        ((dom.offsetWidth - padding * 10) / dom.scrollWidth) * 34
+      }px`;
+    }
+  });
+})();
