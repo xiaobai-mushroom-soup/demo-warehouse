7faf39d7869151e561dfa518d812cd5855dcf061
grz <1605537551@qq.com>
2023-04-12 00:00:26 +0800

    equalization:done

--- pages/flex/js/button.js
+++ pages/flex/js/button.js
@@ -5,7 +5,7 @@
     const padding = parseFloat(paddingLeft) || 0;
     if (dom.scrollWidth > dom.offsetWidth) {
       dom.style.fontSize = `${
-        ((dom.offsetWidth - padding * 20) / dom.scrollWidth) * 24
+        ((dom.offsetWidth - padding * 30) / dom.scrollWidth) * 14
       }px`;
     }
   });
